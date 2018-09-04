77cb3964e2d9cdba1c6458b6a098072aae552a02
Marcel Bargull <marcel.bargull@udo.edu>
2018-09-04 20:49:27 +0200

    only libcryptominisat+pycryptosat; avoid Boost and GPL

--- recipe/build.sh
+++ recipe/build.sh
@@ -16,6 +16,9 @@ cmake \
   -DENABLE_PYTHON_INTERFACE=ON \
   -DFORCE_PYTHON2=`[[ $PY3K == 1 ]] && echo OFF || echo ON` \
   -DENABLE_TESTING=OFF \
+  -DMIT=ON \
+  -DONLY_SIMPLE=ON \
+  -DCMAKE_BUILD_TYPE=Release \
   ..
 
 make -j${CPU_COUNT} install
