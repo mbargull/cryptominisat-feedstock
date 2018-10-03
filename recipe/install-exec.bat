2abc372c464040032c2137db339e95fe8fc26d4e
Marcel Bargull <marcel.bargull@udo.edu>
2018-10-03 20:15:11 +0200

    skip/remove executables on Windows

--- recipe/install-exec.bat
+++ recipe/install-exec.bat
@@ -1,2 +1,4 @@
 COPY %RECIPE_DIR%\install-exec.sh .
 bash install-exec.sh
+REM Running exe fails with exit code 0xC0000135 (STATUS_DLL_NOT_FOUND).
+DEL "%LIBRARY_BIN%\cryptominisat5_simple.exe"
--- recipe/install-py.bat
+++ recipe/install-py.bat
@@ -1,2 +1,4 @@
 COPY %RECIPE_DIR%\install-py.sh .
 bash install-py.sh
+REM Running exe fails with exit code 0xC0000136 (STATUS_DLL_NOT_FOUND).
+DEL "%LIBRARY_BIN%\cryptominisat5_simple.exe"
