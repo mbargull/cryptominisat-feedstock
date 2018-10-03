215976c247fa3b1b0114c2aea86033f7990e58a4
Marcel Bargull <marcel.bargull@udo.edu>
2018-10-03 03:41:02 +0200

    install to %LIBRARY_PREFIX% on Windows

--- recipe/install.sh
+++ recipe/install.sh
@@ -6,11 +6,17 @@ if [[ $(uname) == 'Darwin' ]] && [[ $PY3K == 1 ]]; then
     export LDFLAGS="${LDFLAGS} -undefined dynamic_lookup"
 fi
 
+if [[ ${target_platform} =~ win* ]]; then
+    install_prefix="${LIBRARY_PREFIX}"
+else
+    install_prefix="${PREFIX}"
+fi
+
 # * ENABLE_TESTING: Testing requires lit which is not packaged yet:
 #   https://github.com/conda-forge/staged-recipes/issues/4630
 cmake \
     -G "${CMAKE_GENERATOR}" \
-    -DCMAKE_INSTALL_PREFIX="${PREFIX}" \
+    -DCMAKE_INSTALL_PREFIX="${install_prefix}" \
     -DENABLE_TESTING=OFF \
     -DMIT=ON \
     "${@}" \
